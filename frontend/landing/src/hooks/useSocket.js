a5e3c11620263b559ef543e7dd17fd45b69c871d
its-m4gn3t <its.me.magnet@proton.me>
2025-12-02 22:37:35 +0545

    .

--- frontend/landing/src/hooks/useSocket.js
+++ frontend/landing/src/hooks/useSocket.js
@@ -27,7 +27,7 @@ export const useSocket = () => {
     // });
 const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "https://leosainamina.onrender.com"
+    ? "https://leosainamaina.onrender.com"
     : "http://localhost:5001";
 
 socketRef.current = io(BACKEND_URL, {
